6ba5c9b34080aac76608db5a8ef47b92d5c47820
Yekku <yevgen.liukkonen@gmail.com>
2018-08-20 18:49:13 +0300

    fix readline-sync import and add eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import readlineSync from 'readline-sync';
+import readlineSync from './readline-sync';
 
-console.log('Welcome to the Brain Games!');
-
-// Wait for user's response.
-const userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
+export default readlineSync;
---
+++ src/readline-sync.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+// Wait for user's response.
+const userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+export default readlineSync;
