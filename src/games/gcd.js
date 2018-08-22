81ac2a8895d349cdcd7bf89cb5ca523d39d0a098
Yekku <yevgen.liukkonen@gmail.com>
2018-08-22 23:36:33 +0300

    fix gcd game

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,22 +4,20 @@ import getRandomNumber from '../utils';
 
 const rule = 'Find the greatest common divisor of given numbers.';
 
+const gcd = (n1, n2) => {
+  const dividend = Math.max(n1, n2);
+  const divisor = Math.min(n1, n2);
+  const remainder = dividend % divisor;
+  if (remainder === 0) {
+    return divisor;
+  }
+  return gcd(divisor, remainder);
+};
+
 const greatestCommonDivisor = () => {
   const firstNumber = getRandomNumber(1, 99);
   const secondNumber = getRandomNumber(1, 99);
-
-  const gcd = (n1, n2) => {
-    const dividend = Math.max(n1, n2);
-    const divisor = Math.min(n1, n2);
-    const remainder = dividend % divisor;
-    if (remainder === 0) {
-      return divisor;
-    }
-    return gcd(divisor, remainder);
-  };
-
   const result = gcd(firstNumber, secondNumber);
-
   const question = `${firstNumber} ${secondNumber}`;
   const answer = `${result}`;
 
