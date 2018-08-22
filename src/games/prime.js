4bce01761a9f261e1b3220b0bcdc88927dc8af20
Yekku <yevgen.liukkonen@gmail.com>
2018-08-22 23:24:09 +0300

    add prime game

---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+import { cons } from 'hexlet-pairs';
+import gameFlow from '..';
+import getRandomNumber from '../utils';
+
+const rule = 'Is this number prime?';
+
+const isPrime = (num) => {
+  if (num < 2 || (num % 2 === 0 && num !== 2)) {
+    return false;
+  }
+  const maxDiv = Math.round(Math.sqrt(num)) + 1;
+  for (let i = 3; i < maxDiv; i += 2) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const prime = () => {
+  const question = getRandomNumber(1, 30);
+  const answer = isPrime(question) ? 'yes' : 'no';
+  return cons(question, answer);
+};
+
+const game = () => gameFlow(prime, rule);
+
+export default game;
