f0b18279d863c05db964187ddb0fabc43426bf64
Yekku <yevgen.liukkonen@gmail.com>
2018-08-22 10:54:42 +0300

    add balance game

---
+++ src/games/balance.js
@@ -0,0 +1,28 @@
+// Balance game logic
+import { cons } from 'hexlet-pairs';
+import { newGame, getRandomNumber } from '..';
+
+const rule = 'Balance the given number.';
+
+const balance = () => {
+  const number = getRandomNumber(100, 1000);
+  let numberToArr = number.toString().split('').sort();
+  let correctAnswer = '';
+  while (correctAnswer === '') {
+    const maxNum = Math.max(...numberToArr);
+    const minNum = Math.min(...numberToArr);
+    if ((maxNum - minNum) <= 1) {
+      correctAnswer = numberToArr.sort().join('');
+    }
+    numberToArr = numberToArr.sort().slice(1, numberToArr.length - 1);
+    numberToArr.push(maxNum - 1);
+    numberToArr.push(minNum + 1);
+  }
+
+  const question = `${number}`;
+  const answer = `${correctAnswer}`;
+
+  return cons(question, answer);
+};
+
+export default () => newGame(balance, rule);
